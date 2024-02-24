6b7e2c3e742707107e85a95a755a2225d484cfd4
marlonair <marlonair@laptopvarlonair.home>
2024-02-24 07:10:32 +0100

    addidn parallax

--- src/components/sections/Parallax/Parallax.tsx
+++ src/components/sections/Parallax/Parallax.tsx
@@ -1,28 +1,59 @@
-import "./Parallax.scss";
-import { fireballOne, fireballThree, fireballTwo } from "../../../assets";
+import './Parallax.scss';
+import {
+  fireLandScapeBackground,
+  fireballOne,
+  fireballThree,
+  fireballTwo
+} from '../../../assets';
+
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
+
 interface ParallaxProps {
   // TODO
   type: string;
 }
 
 const Parallax = ({ type }: ParallaxProps) => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start start', 'end start']
+  });
+  const yBg = useTransform(scrollYProgress, [0, 1], ['0%', '100%']);
+  const yText = useTransform(scrollYProgress, [0, 1], ['0%', '100%']);
   return (
     <div
       className={`grid--parallax grid parallax parallax--${
-        type === "services" ? "services" : "journey"
+        type === 'services' ? 'services' : 'journey'
       } `}
+      ref={ref}
     >
-      <h2>{type === "services" ? "My Mission" : "My Journey"}</h2>
-      <div className="landscape"></div>
-      <div className="fireball fireball--1">
-        <img src={fireballTwo} alt="" />
-      </div>
-      <div className="fireball fireball--2">
-        <img src={fireballOne} alt="" />
-      </div>
-      <div className="fireball fireball--3">
-        {<img src={fireballThree} alt="" />}
-      </div>
+      <div className='overlay'></div>
+      <motion.h2 style={{ y: yBg }}>
+        {type === 'services' ? 'My Mission' : 'My Journey'}
+      </motion.h2>
+      <img
+        src={fireLandScapeBackground}
+        alt=''
+        className='landscape__img'
+      />
+      <motion.img
+        style={{ y: yText }}
+        src={fireballTwo}
+        alt=''
+        className='fireball__img fireball__img--1'
+      />
+      <img
+        src={fireballOne}
+        alt=''
+        className='fireball__img fireball__img--2'
+      />
+      <img
+        src={fireballThree}
+        alt=''
+        className='fireball__img fireball__img--3'
+      />
     </div>
   );
 };
