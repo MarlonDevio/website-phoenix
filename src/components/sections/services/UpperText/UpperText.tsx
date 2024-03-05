ca92dc2a4ff8bb3f219cadadd734722b1e77e722
Marlon Hamaekers <marlon@marlon-m3.home>
2024-03-06 00:09:53 +0100

    fixed apost

--- src/components/sections/services/UpperText/UpperText.tsx
+++ src/components/sections/services/UpperText/UpperText.tsx
@@ -5,7 +5,7 @@ const UpperText = () => {
   return (
     <MotionContainer className="relative flex items-center justify-end">
       <p className="absolute right-[20%] hidden md:block  ">
-        Empowering your brand's evolution
+        Empowering your brand&apos;s evolution
         <br /> with tech-inspired resilience
       </p>
       <hr className="hidden min-w-[30%] md:block" />
