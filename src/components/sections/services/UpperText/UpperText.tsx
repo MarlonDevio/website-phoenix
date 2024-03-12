0e8654a6882a7de9a0204c725213c3bcf2c949d0
Marlon Hamaekers <marlon@marlon-m3.home>
2024-03-12 01:24:41 +0100

    fixed title upper text services

--- src/components/sections/services/UpperText/UpperText.tsx
+++ src/components/sections/services/UpperText/UpperText.tsx
@@ -4,16 +4,16 @@ import { motion } from "framer-motion";
 const UpperText = () => {
   return (
     <MotionContainer
-      className="relative  hidden items-center justify-end md:flex"
+      className="relative  hidden items-center justify-end sm:flex"
       variants={fadeInOnView}
       whileInView="whileInView"
       initial="initial"
     >
-      <motion.p className="absolute right-[20%] text-[1.8rem]   ">
+      <motion.p className="absolute right-[10%] text-[1.8rem]   ">
         Empowering your brand&apos;s evolution
         <br /> with tech-inspired resilience
       </motion.p>
-      <hr className="hidden min-w-[30%] md:block" />
+      <hr className=" right-[-30%] hidden min-w-[30%] sm:block lg:absolute lg:min-w-[50%]" />
     </MotionContainer>
   );
 };
