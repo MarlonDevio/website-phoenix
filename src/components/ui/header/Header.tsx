4a67eab97eede4d62285ee77910a332304e653f0
MarlonHamaekersPXL <mar.hamaekers@gmail.com>
2024-03-21 17:52:11 +0100

    restructure app

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { useEffect, useRef } from "react";
 import { bg1, bg2, bg3, walkingToPhoenix } from "../../../../public/assets";
 import Letter from "./Letter";
@@ -17,67 +16,184 @@ gsap.registerPlugin(useGSAP, ScrollTrigger);
 const lettersMarlon = ["m", "a", "r", "l", "o", "n"];
 const lettersDev = ["d", "e", "v", ".", "i", "o"];
 
+function updateTotalBodyHeight(contentHolder: any, totalBodyHeight: any) {
+  const contentHolderHeight = contentHolder.current
+    ? contentHolder.current.offsetHeight
+    : 0;
+  const imgHolderHeight = window.innerHeight;
+  const additionalScrollHeight = window.innerHeight;
+
+  totalBodyHeight.current =
+    contentHolderHeight + imgHolderHeight + additionalScrollHeight;
+}
+
 export default function Header() {
   const contentHolderRef = useRef<HTMLDivElement>();
-  const imgHolderRef = useRef<HTMLDivElement>();
+  const imgHolderRef = useRef<HTMLDivElement>(null);
   const totalBodyHeight = useRef<number>();
-  // const additionalScrollHeight = window.innerHeight;
+  const websiteContent = useRef<HTMLDivElement>(null);
 
-  // useEffect(() => {
+  //     // Forceer ScrollTrigger om de lay-out te heroverwegen
+  //     ScrollTrigger.refresh();
+  //   }
+
+  //   window.addEventListener("resize", updateTotalBodyHeight);
+  //   window.addEventListener("resize", (event) => ScrollTrigger.refresh);
+
+  //   setupAnimations();
+
+  //   return () => {
+  //     window.removeEventListener("resize", updateTotalBodyHeight);
+  //     window.removeEventListener("resize", (event) => ScrollTrigger.refresh);
+  //     ScrollTrigger.clearScrollMemory();
+  //     ScrollTrigger.getAll().forEach((st) => st.kill()); // Opruimen wanneer de component unmount
+  //   };
+  // }, []);
+  useGSAP(() => {
+    function updateTotalBodyHeight() {
+      const contentHolderHeight = contentHolderRef.current
+        ? contentHolderRef.current.offsetHeight
+        : 0;
+      const imgHolderHeight = window.innerHeight; // Neem aan dat dit correct is voor jouw layout
+      const additionalScrollHeight = window.innerHeight; // Dit ook
+
+      totalBodyHeight.current =
+        contentHolderHeight + imgHolderHeight + additionalScrollHeight;
+
+      ScrollTrigger.refresh(true); // Ververs alle ScrollTriggers
+    }
+    function setUpAnimations() {
+      updateTotalBodyHeight();
+      ScrollTrigger.create({
+        trigger: websiteContent.current,
+        start: "-0.1% top",
+        end: "bottom bottom",
+        markers: true,
+        onEnter: () => {
+          gsap.set(websiteContent.current, {
+            position: "absolute",
+            top: "195%",
+          });
+        },
+        onLeaveBack: () => {
+          gsap.set(websiteContent.current, {
+            position: "fixed",
+            top: "0",
+          });
+        },
+      });
+
+      gsap.to(".header .letters:first-child", {
+        x: () => -innerWidth * 3,
+        scale: 10,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          start: "top top",
+          end: `+=200%`,
+          scrub: 1,
+        },
+      });
+      gsap.to(".header .letters:last-child", {
+        x: () => innerWidth * 3,
+        scale: 10,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          start: "top top",
+          end: `+=200%`,
+          scrub: 1,
+        },
+      });
+
+      gsap.to(imgHolderRef.current, {
+        rotation: 0,
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0 100%)",
+        ease: "power2.inOut",
+        scrollTrigger: {
+          start: "top top",
+          end: `+=200%`,
+          scrub: 1,
+        },
+      });
+
+      gsap.to(imgHolderRef.current.querySelector("img"), {
+        scale: 1,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          start: "top top",
+          end: `+=200%`,
+          scrub: 1,
+        },
+      });
+      ScrollTrigger.refresh();
+      console.log(totalBodyHeight.current);
+    }
+    window.addEventListener("resize", updateTotalBodyHeight);
+
+    setUpAnimations();
+
+    return () => {
+      window.removeEventListener("resize", updateTotalBodyHeight);
+      ScrollTrigger.getAll().forEach((st) => st.kill()); // Opruimen wanneer de component unmount
+      ScrollTrigger.clearScrollMemory();
+    };
+  }, []);
+  useEffect(() => {
+    function handleResize() {
+      totalBodyHeight.current = contentHolderRef.current
+        ? contentHolderRef.current.offsetHeight + window.innerHeight * 2
+        : 0;
+      ScrollTrigger.refresh();
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Call initially in case there are immediate changes
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  // useGSAP(() => {
   //   const contentHolder = contentHolderRef.current;
   //   const imgHolder = window.innerHeight;
   //   const additionalScrollHeight = window.innerHeight;
 
   //   totalBodyHeight.current =
-  //     contentHolder?.offsetHeight &&
   //     contentHolder.offsetHeight + imgHolder + additionalScrollHeight;
-
-  //   if (contentHolderRef.current) {
-  //     console.log("ContentHolder: " + contentHolderRef.current.offsetHeight);
-  //   }
-  //   if (imgHolderRef.current) {
-  //     console.log("ImgHolder: " + imgHolderRef.current.offsetHeight);
-  //   }
-  //   if (window) {
-  //     console.log("Window: " + window.innerHeight);
-  //     console.log("Total: " + totalBodyHeight.current);
-  //   }
-  //   // console.log(additionalScrollHeight);
+  //   const timeline = gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: ".website-content",
+  //       start: "-0.1% top",
+  //       end: "bottom bottom",
+  //       markers: true,
+  //       onEnter: () => {
+  //         gsap.set(".website-content", { position: "absolute", top: "195%" });
+  //       },
+  //       onLeaveBack: () => {
+  //         gsap.set(".website-content", { position: "fixed", top: "0" });
+  //       },
+  //     },
+  //   });
+  //   timeline.to(".header .letters:first-child", {
+  //     x: () => -innerWidth * 3,
+  //     scale: 10,
+  //     ease: "power2.inOut",
+  //     scrollTrigger: {
+  //       start: "top top",
+  //       end: `+=200%`,
+  //       scrub: 1,
+  //     },
+  //   });
+  //   timeline.to(".header .letters:last-child", {
+  //     x: () => innerWidth * 3,
+  //     scale: 10,
+  //     ease: "power2.inOut",
+  //     scrollTrigger: {
+  //       start: "top top",
+  //       end: `+=200%`,
+  //       scrub: 1,
+  //     },
+  //   });
   // });
-
-  useGSAP(() => {
-    const contentHolder = contentHolderRef.current;
-    const imgHolder = window.innerHeight;
-    const additionalScrollHeight = window.innerHeight;
-
-    totalBodyHeight.current =
-      contentHolder?.offsetHeight &&
-      contentHolder.offsetHeight + imgHolder + additionalScrollHeight;
-    const timeline = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".website-content",
-        start: "-0.1% top",
-        end: "bottom bottom",
-        markers: true,
-        onEnter: () => {
-          gsap.set(".website-content", { position: "absolute", top: "195%" });
-        },
-        onLeaveBack: () => {
-          gsap.set(".website-content", { position: "fixed", top: "0" });
-        },
-      },
-    });
-    timeline.to(".header .letters:first-child", {
-      x: () => -innerWidth * 3,
-      scale: 10,
-      ease: "power2.inOut",
-      scrollTrigger: {
-        start: "top top",
-        end: `+=200%`,
-        scrub: 1,
-      },
-    });
-  }, []);
   // https://www.youtube.com/watch?v=AaO-LmExmkM&list=PLHehj03wDKX8wsi_zuk_JTSEZSFtf20XJ&index=6&ab_channel=Codegrid 5.52min
   return (
     <div
@@ -95,7 +211,11 @@ export default function Header() {
         <Letters letterList={lettersDev} className={"letters"} />
       </header>
 
-      <div className="website-content fixed top-0 min-h-[100vh] w-[100%] bg-white">
+      <div
+        // @ts-ignore
+        ref={websiteContent}
+        className="website-content fixed top-0 min-h-[100vh] w-[100%] bg-white"
+      >
         {/*  @ts-ignore*/}
         <ImageHolder ref={imgHolderRef} />
         {/*  @ts-ignore*/}
